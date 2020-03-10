e35845512a8fbd100c4807d23d64ea5960689606
Andrii Tkach <andrii.tkach@vilmate.com>
2020-03-10 17:33:38 +0200

    Initial commit

---
+++ WiredBrainCoffee.AdminApp/Settings/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace WiredBrainCoffee.AdminApp.Settings
+{
+  class AppSettings
+  {
+        public static readonly string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=iccoffee;AccountKey=TX6NnS/0mZhQB+B08az5FYwgyu6xy1glwzOfAXVMqlKJNVHYD0lrpfyvd8ZOHowX4hYpapYiiDHLa2FheGVD0A==;EndpointSuffix=core.windows.net";
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+1. Before you run the UWP application, please set the connection string for your Storage Account in the file
+WiredBrainCoffee.AdminApp\Settings\AppSettings.cs
+
+2. Before you run the ASP.NET Core web application, please set the connection string for your Storage Account in the file
+WiredBrainCoffee.WebApp\appsettings.json
